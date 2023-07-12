77f34b879e0e755ae467e8623b8e15bae514e314
Yael Hasson <yaelandrea@google.com>
2023-07-12 03:32:49 +0000

    adding alfonso code in gitlab

---
+++ proyecto-vs2019-swagger-main/ApiSwagger/ApiSwagger/Controllers/PeliculasController.cs
@@ -0,0 +1,116 @@
+﻿using ApiSwagger.Context;
+using ApiSwagger.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiSwagger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeliculasController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        public PeliculasController(AppDbContext context)
+        {
+            this.context = context;
+        }
+        // GET: api/<PeliculasController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(context.Movie.ToList());
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // GET api/<PeliculasController>/5
+        [HttpGet("{id}", Name="GetPelicula")]
+        public ActionResult Get(int id)
+        {
+            try
+            {
+                var Pelicula = context.Movie.FirstOrDefault(f => f.id == id);
+                return Ok(Pelicula);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            //return "value";
+        }
+
+        // POST api/<PeliculasController>
+        [HttpPost]
+        public ActionResult Post([FromBody]Peliculas peliculas)
+        {
+            try
+            {
+                context.Movie.Add(peliculas);
+                context.SaveChanges();
+                return CreatedAtRoute("GetPelicula", new { id= peliculas.id }, peliculas);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/<PeliculasController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Peliculas peliculas)
+        {
+            try
+            {
+                if(peliculas.id == id)
+                {
+                    context.Entry(peliculas).State = EntityState.Modified;
+                    context.SaveChanges();
+                    return CreatedAtRoute("GetPelicula", new { id = peliculas.id }, peliculas);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<PeliculasController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id, [FromBody] Peliculas peliculas)
+        {
+            try
+            {
+                var pelicula = context.Movie.FirstOrDefault(f => f.id == id);
+                if (pelicula != null)
+                {
+                    context.Movie.Remove(pelicula);
+                    context.SaveChanges();
+                    return Ok(id);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ proyecto-vs2019-swagger-main/ApiSwagger/ApiSwagger/Models/Peliculas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiSwagger.Models
+{
+    public class Peliculas
+    {
+        [Key]
+        public int id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+        public string Rating { get; set; }
+    }
+}
